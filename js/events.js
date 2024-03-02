ffdc2ed7fcce27d383e8417fea10ba55e457949f
nfaciano <nickfac88@gmail.com>
2024-03-02 13:26:05 -0500

    test

--- js/events.js
+++ js/events.js
@@ -1,13 +1,3 @@
 $(document).ready(function() {
-    // Optionally, automatically load HTML events when the page loads
-    // loadHtmlEvents();
-
-    // Binding event handlers for buttons
-    $('#loadHtmlBtn').click(loadHtmlEvents); // Handler for loading HTML content directly
-
-    $('#loadXmlBtn').click(loadXmlEvents); // Handler for loading XML content
-
-    $('#loadJsonBtn').click(loadJsonEvents); // Handler for loading JSON content
-
-    $('#loadHtmlJQueryBtn').click(loadHtmlWithJQuery); // Handler for loading HTML content with jQuery effects
+    loadHtmlEvents();
 });

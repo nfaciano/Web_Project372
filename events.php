9e8862a704243cfe89c8cb7e139e50253c4155bb
nfaciano <nickfac88@gmail.com>
2024-04-06 12:22:32 -0400

    changes2

--- events.php
+++ events.php
@@ -12,11 +12,11 @@
     <?php include 'Event.php'; ?>
     <img src="images/logo.png" alt="Lambda Chi Alpha Logo" width="150" height="150">
     <nav>
-        <ul>
+    <ul>
             <li><a href="index.html">Home</a></li>
             <li><a href="aboutus.html">About Us</a></li>
-            <li><a href="events.html">Events</a></li>
-            <li><a href="merchandise.html">Merchandise</a></li>
+            <li><a href="events.php">Events</a></li>
+            <li><a href="merchandise.php">Merchandise</a></li>
             <li><a href="donate.html">Donate</a></li>
             <li><a href="contactus.html">Contact Us</a></li>
         </ul>
@@ -28,7 +28,7 @@
      <!-- Events Content -->
      <section id="eventsList">
     <?php
-    $events = [new Event("Charity Run", "2023-04-30", "Campus Grounds", "Join us for a run!")];
+    $events = [new Event("Charity Run", "2023-04-30", "Campus Grounds", "Join us for a run ride!")];
     foreach ($events as $event) {
         echo "<div>";
         echo "<h3>" . htmlspecialchars($event->title) . "</h3>";
